1373af0d97329d3ce2d802d81f477fcfe21455a1
Digostirod <digostirod@gmail.com>
2025-04-22 21:03:55 -0300

    Delete src directory

--- src/components/ProcessoDetail.jsx
+++
@@ -1,7 +0,0 @@
-// Importações necessárias para o componente ProcessoDetail
-import { pythonBridge } from '../app/api/python-bridge';
-import Button from './Button';
-import Card from './Card';
-import Alert from './Alert';
-import Input from './Input';
-import MarkdownRenderer from './MarkdownRenderer';
